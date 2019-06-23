f7fd7fd646e3e04aa03368338ce27da8bb1d642c
Gregory John Casamento <greg.casamento@gmail.com>
2019-06-22 21:09:57 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tools-bridge
+
+The purpose of this tool is to allow for users to analyze the libraries which comprise GNUstep and automatically be able to metadata regarding classes and method signatures so that they can use that information to create software which will allow them to create a bridge to Cocoa.
---
+++ generate_bridge/generate_bridge/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  generate_bridge
+//
+//  Created by Gregory John Casamento on 6/22/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
